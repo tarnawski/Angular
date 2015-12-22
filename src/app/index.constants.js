d0dff7625d6237a1472809fb05c92fb1516364a1
Tomasz Tarnawski (PGS Software) <ttarnawski@pgs-soft.com>
2015-12-22 10:05:50 +0100

    Initial commit

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('iBrowser')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
