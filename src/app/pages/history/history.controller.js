edcac5dcbe76fe59b68d1d166192f53697ce86a1
ttarnawski <ttranawski@pgs-soft.com>
2016-01-10 22:44:14 -0800

    Add posibility to clear all history

--- src/app/pages/history/history.controller.js
+++ src/app/pages/history/history.controller.js
@@ -1,28 +1,31 @@
-(function ()
-{
-    'use strict';
+(function () {
+  'use strict';
 
-    angular
-        .module('iBrowser.history')
-        .controller('HistoryController', HistoryController);
+  angular
+    .module('iBrowser.history')
+    .controller('HistoryController', HistoryController);
 
-    /** @ngInject */
-    function HistoryController($scope, store, songsDataService)
-    {
-      var vm = this;
-      if (store.get('history')) {
-        var history = store.get('history');
+  /** @ngInject */
+  function HistoryController($scope, store, songsDataService) {
+    var vm = this;
+    if (store.get('history')) {
+      var history = store.get('history');
 
-        songsDataService.getSongInformation().search({id: history.toString()}, function (data) {
-          var songs = data.results;
-          vm.songs = songs;
-        });
-      }
-
-      vm.delete = function(index){
-        vm.songs.splice(index, 1);
-        history.splice(index, 1);
-        store.set('history', history);
-      };
+      songsDataService.getSongInformation().search({id: history.toString()}, function (data) {
+        var songs = data.results;
+        vm.songs = songs;
+      });
     }
+
+    vm.delete = function (index) {
+      vm.songs.splice(index, 1);
+      history.splice(index, 1);
+      store.set('history', history);
+    };
+
+    vm.deleteAll = function () {
+      vm.songs = [];
+      store.remove('history');
+    };
+  }
 })();
